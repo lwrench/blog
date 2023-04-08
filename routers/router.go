ae9a6ce3073854f3d9dab99e8d7b20f221d1b20a
lwrench <1243115181@qq.com>
2023-04-08 16:03:31 +0800

    feat: git ignore

--- routers/router.go
+++ routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/lwrench/blog/pkg/setting"
+	"github.com/lwrench/blog/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
@@ -19,6 +20,21 @@ func InitRouter() *gin.Engine {
 			"message": "test",
 		})
 	})
+
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.GET("/articles", v1.GetArticles)
+		apiv1.GET("/article/:id", v1.GetArticle)
+		apiv1.POST("/article", v1.AddArticle)
+		apiv1.PUT("/article/:id", v1.EditArticle)
+		apiv1.DELETE("/article/:id", v1.DeleteArticle)
+
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.GET("/tag/:id", v1.GetTag)
+		apiv1.POST("/tag", v1.AddTag)
+		apiv1.PUT("/tag/:id", v1.EditTag)
+		apiv1.DELETE("/tag/:id", v1.DeleteTag)
+	}
 
 	return r
 }
