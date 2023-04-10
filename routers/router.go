c077a65fbe28e109b87dc0c7d0c39882c2ba5c25
lwrench <1243115181@qq.com>
2023-04-11 00:13:43 +0800

    feat: crud article

--- routers/router.go
+++ routers/router.go
@@ -24,10 +24,10 @@ func InitRouter() *gin.Engine {
 	apiv1 := r.Group("/api/v1")
 	{
 		apiv1.GET("/articles", v1.GetArticles)
-		apiv1.GET("/article/:id", v1.GetArticle)
-		apiv1.POST("/article", v1.AddArticle)
-		apiv1.PUT("/article/:id", v1.EditArticle)
-		apiv1.DELETE("/article/:id", v1.DeleteArticle)
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		apiv1.POST("/articles", v1.AddArticle)
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 
 		apiv1.GET("/tags", v1.GetTags)
 		apiv1.POST("/tags", v1.AddTag)
